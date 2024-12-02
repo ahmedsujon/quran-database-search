3280a791fe220b852580072ef3d6ebd144b1d1ac
Sujon Ahmed <sujonahmed424@gmail.com>
2024-12-02 22:02:54 +0600

    error fixed

--- app/Imports/HadithImport.php
+++ app/Imports/HadithImport.php
@@ -4,17 +4,17 @@ namespace App\Imports;
 
 use App\Models\Hadith;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class HadithImport implements ToModel
+class HadithImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        dd($row);
-
+        // dd($row);
         $hadithData = new Hadith();
-        $hadithData->group_name                          = $row['group_name'];
-        $hadithData->hadith_english                      = $row['hadith_english'];
-        $hadithData->hadith_arabic                       = $row['hadith_arabic'];
+        $hadithData->group_name                          = isset($row['group_name'])?$row['group_name'] : null;
+        $hadithData->hadith_english                      = isset($row['hadith_english'])?$row['hadith_english'] : null;
+        $hadithData->hadith_arabic                       = isset($row['hadith_arabic'])?$row['hadith_arabic'] : null;
         $hadithData->save();
     }
 }
