fa76e96e23b8665a0419e0d949b0869bbeb91dd5
Sujon Ahmed <sujonahmed424@gmail.com>
2024-12-22 21:01:06 +0600

    updates

--- resources/views/livewire/app/queries-search/query-search-result-component.blade.php
+++ resources/views/livewire/app/queries-search/query-search-result-component.blade.php
@@ -1,21 +1,53 @@
 <div>
     <div class="container mt-4">
-        <table class="table table-striped table-bordered">
+        <!-- Table -->
+        <table class="table table-striped table-bordered mt-5">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">Word Or Category</th>
-                    <th scope="col">Summary Description</th>
-                    <th scope="col">Verse Description</th>
-                    <th scope="col">Inference Flag</th>
+                    <th class="text-center" scope="col" style="width: 10%;">Word Or Category</th>
+                    <th scope="col" style="width: 30%;">Summary Description</th>
+                    <th scope="col" style="width: 45%;">Verse Description</th>
+                    <th class="text-center" scope="col" style="width: 10%;">Inference Flag</th>
+                    <th class="text-center" scope="col" style="width: 5%;">Hadith Reference</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($final_results as $item)
                     <tr>
-                        <td scope="row">{{ $item->word_topic }}</td>
-                        <td scope="row">1</td>
-                        <td scope="row">1</td>
-                        <td scope="row">1</td>
+                        <td scope="row" style="width: 10%;">{{ $item->word_topic }}</td>
+                        <td style="width: 30%;">{{ $item->ayat_summary_des }}</td>
+                        <td style="width: 50%;">{{ $item->quran_english }}</td>
+                        <td style="width: 10%;">{{ $item->inferance_flag }}</td>
+                        <td style="width: 10%;" class="text-center">
+                            <button class="btn btn-info btn-sm" data-toggle="modal"
+                                data-target="#modal-{{ $item->id }}">
+                                Read
+                            </button>
+
+                            <!-- Modal -->
+                            <div class="modal fade" id="modal-{{ $item->id }}" tabindex="-1" role="dialog"
+                                aria-labelledby="modalLabel-{{ $item->id }}" aria-hidden="true">
+                                <div class="modal-dialog modal-lg" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="modalLabel-{{ $item->id }}">Hadith
+                                                Information</h5>
+                                            <button type="button" class="close" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            {{ $item->hadith_english }}
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
@@ -24,4 +56,9 @@
             {{ $final_results->links('livewire.app-pagination') }}
         </nav>
     </div>
-</div>
+
+    @push('scripts')
+        <!-- Bootstrap JS and dependencies -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    @endpush
