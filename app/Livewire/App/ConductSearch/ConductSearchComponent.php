6fd691ee7dab66476b245ec5cfa15aa48e065eeb
ahmedsujon <sujonahmed424@gmail.com>
2024-12-23 09:36:49 +0600

    change

--- app/Livewire/App/ConductSearch/ConductSearchComponent.php
+++ app/Livewire/App/ConductSearch/ConductSearchComponent.php
@@ -15,16 +15,16 @@ class ConductSearchComponent extends Component
     public function render()
     {
 
-        $querySearchResults = Word::join('qurans', 'words.surah_ayat', '=', 'qurans.surah_ayat')
-            ->select('words.*', 'qurans.*')
-            ->where('words.word_topic', 'like', '%' . $this->searchTerm . '%')
-            ->paginate($this->sortingValue);
-
         // $querySearchResults = Word::join('qurans', 'words.surah_ayat', '=', 'qurans.surah_ayat')
-        //     ->join('hadiths', 'words.word_topic', '=', 'hadiths.group_name') // Join the hadiths table based on word_topic
-        //     ->select('words.*', 'qurans.*', 'hadiths.*') // Select fields from all joined tables
-        //     ->where('words.word_topic', 'like', '%' . $this->searchTerm . '%') // Filter based on the search term
+        //     ->select('words.*', 'qurans.*')
+        //     ->where('words.word_topic', 'like', '%' . $this->searchTerm . '%')
         //     ->paginate($this->sortingValue);
+
+        $querySearchResults = Word::join('qurans', 'words.surah_ayat', '=', 'qurans.surah_ayat')
+            ->join('hadiths', 'words.word_topic', '=', 'hadiths.group_name') // Join the hadiths table based on word_topic
+            ->select('words.*', 'qurans.*', 'hadiths.*') // Select fields from all joined tables
+            ->where('words.word_topic', 'like', '%' . $this->searchTerm . '%') // Filter based on the search term
+            ->paginate($this->sortingValue);
 
         return view('livewire.app.conduct-search.conduct-search-component', ['querySearchResults' => $querySearchResults])
             ->layout('livewire.app.layouts.base');
