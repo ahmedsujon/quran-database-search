00105222697832a0d75b108c3a73eea40964c5e4
ahmedsujon <sujonahmed424@gmail.com>
2025-01-06 12:04:36 +0600

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(SettingTableSeeder::class);
+        $this->call(MAinMenuSeeder::class);
     }
 }
